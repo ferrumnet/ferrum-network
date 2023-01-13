57d7936d3fa7e6de296dedd91cd4beea9d8981cf
3ierratango <118272976+3ierratango@users.noreply.github.com>
2023-01-13 16:55:08 +0400

    add testnet chainspec

--- node/src/command.rs
+++ node/src/command.rs
@@ -56,6 +56,7 @@ impl SubstrateCli for Cli {
     fn load_spec(&self, id: &str) -> Result<Box<dyn sc_service::ChainSpec>, String> {
         Ok(match id {
             "dev" => Box::new(chain_spec::development_config(self)?),
+            "alpha-testnet" => Box::new(chain_spec::alpha_testnet_config(self)?),
             "" | "local" => Box::new(chain_spec::local_testnet_config(self)?),
             path => Box::new(chain_spec::ChainSpec::from_json_file(
                 std::path::PathBuf::from(path),

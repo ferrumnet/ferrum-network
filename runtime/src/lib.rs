fefd50a69e3f891c47035544e29e1bf79c1f1831
3ierratango <118272976+3ierratango@users.noreply.github.com>
2023-01-24 12:46:26 +0400

    update chainspec

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -110,8 +110,8 @@ pub mod opaque {
 }
 
 pub const VERSION: RuntimeVersion = RuntimeVersion {
-    spec_name: create_runtime_str!("ferrum-x-runtime"),
-    impl_name: create_runtime_str!("ferrum-x-runtime"),
+    spec_name: create_runtime_str!("ferrum-runtime"),
+    impl_name: create_runtime_str!("ferrum-runtime"),
     authoring_version: 1,
     spec_version: 1,
     impl_version: 1,

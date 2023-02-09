58f0f25a2c89a51d62d09218e3371b6024325785
3ierratango <118272976+3ierratango@users.noreply.github.com>
2023-02-09 13:41:22 +0400

    finalize and mining works

--- scripts/start-local-testnet.sh
+++ scripts/start-local-testnet.sh
@@ -3,16 +3,16 @@
 rm -rf ./chain
 
 # generate chain spec
-./target/release/ferrum-x-network build-spec --disable-default-bootnode > ferrum-local-testnet.json
+./target/release/ferrum-network build-spec --disable-default-bootnode > ferrum-local-testnet.json
 
 # insert the signing keys for alice
-./target/release/ferrum-x-network key insert --key-type ofsg --scheme ecdsa --base-path ./chain/alice --chain ferrum-local-testnet.json --suri //Alice
+./target/release/ferrum-network key insert --key-type ofsg --scheme ecdsa --base-path ./chain/alice --chain ferrum-local-testnet.json --suri //Alice
 
 # insert the signing keys for bob
-./target/release/ferrum-x-network key insert --key-type ofsg --scheme ecdsa --base-path ./chain/bob --chain ferrum-local-testnet.json --suri //Bob
+./target/release/ferrum-network key insert --key-type ofsg --scheme ecdsa --base-path ./chain/bob --chain ferrum-local-testnet.json --suri //Bob
 
 # start Alice node in background
-./target/release/ferrum-x-network --chain ferrum-local-testnet.json --alice --base-path ./chain/alice --ws-port 9944 --config-file-path ./alice_node_config.json &
+./target/release/ferrum-network --chain ferrum-local-testnet.json --alice --base-path ./chain/alice --ws-port 9944 --config-file-path ./alice_node_config.json &
 
 # start Bob node
-./target/release/ferrum-x-network --chain ferrum-local-testnet.json --bob --base-path ./chain/bob --ws-port 9945 --config-file-path ./bob_node_config.json
+./target/release/ferrum-network --chain ferrum-local-testnet.json --bob --base-path ./chain/bob --ws-port 9945 --config-file-path ./bob_node_config.json

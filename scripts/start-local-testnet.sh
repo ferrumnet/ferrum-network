193d3a96816d02f4aa2996d4795204f9cd0e75bd
3ierratango <118272976+3ierratango@users.noreply.github.com>
2023-04-21 17:11:39 +0400

    temp change for testnet

--- scripts/start-local-testnet.sh
+++ scripts/start-local-testnet.sh
@@ -1,15 +1,15 @@
 
-# remove any existing data from chain
-rm -rf ./chain
+# # remove any existing data from chain
+# rm -rf ./chain
 
-# generate chain spec
-./target/release/ferrum-network build-spec --disable-default-bootnode > ferrum-local-testnet.json
+# # generate chain spec
+# ./target/release/ferrum-network build-spec --disable-default-bootnode > ferrum-local-testnet.json
 
-# insert the signing keys for alice
-./target/release/ferrum-network key insert --key-type ofsg --scheme ecdsa --base-path ./chain/alice --chain ferrum-local-testnet.json --suri //Alice
+# # insert the signing keys for alice
+# ./target/release/ferrum-network key insert --key-type ofsg --scheme ecdsa --base-path ./chain/alice --chain ferrum-local-testnet.json --suri //Alice
 
-# insert the signing keys for bob
-./target/release/ferrum-network key insert --key-type ofsg --scheme ecdsa --base-path ./chain/bob --chain ferrum-local-testnet.json --suri //Bob
+# # insert the signing keys for bob
+# ./target/release/ferrum-network key insert --key-type ofsg --scheme ecdsa --base-path ./chain/bob --chain ferrum-local-testnet.json --suri //Bob
 
 # start relaychain and parachain in background
 polkadot-launch ./scripts/polkadot-launch/config.json

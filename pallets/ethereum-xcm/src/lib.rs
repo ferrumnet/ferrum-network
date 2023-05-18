12d2eefbc020d1ce98dce16b96fedee2d322ac47
3ierratango <118272976+3ierratango@users.noreply.github.com>
2023-05-18 13:46:58 +0400

    linting

--- pallets/ethereum-xcm/src/lib.rs
+++ pallets/ethereum-xcm/src/lib.rs
@@ -48,11 +48,11 @@ use ethereum_types::{H160, U256};
 use fp_ethereum::{TransactionData, ValidatedTransaction};
 use fp_evm::{CheckEvmTransaction, CheckEvmTransactionConfig, InvalidEvmTransactionError};
 use frame_support::{
-	codec::{Decode, Encode, MaxEncodedLen},
-	dispatch::{DispatchResultWithPostInfo, Pays, PostDispatchInfo},
-	scale_info::TypeInfo,
-	traits::{EnsureOrigin, Get},
-	weights::Weight,
+    codec::{Decode, Encode, MaxEncodedLen},
+    dispatch::{DispatchResultWithPostInfo, Pays, PostDispatchInfo},
+    scale_info::TypeInfo,
+    traits::{EnsureOrigin, Get},
+    weights::Weight,
 };
 use frame_system::pallet_prelude::OriginFor;
 use pallet_evm::{AddressMapping, GasWeightMapping};
@@ -60,100 +60,100 @@ use sp_runtime::{traits::UniqueSaturatedInto, DispatchErrorWithPostInfo, Runtime
 use sp_std::{marker::PhantomData, prelude::*};
 
 pub use ethereum::{
-	AccessListItem, BlockV2 as Block, LegacyTransactionMessage, Log, ReceiptV3 as Receipt,
-	TransactionAction, TransactionV2 as Transaction,
+    AccessListItem, BlockV2 as Block, LegacyTransactionMessage, Log, ReceiptV3 as Receipt,
+    TransactionAction, TransactionV2 as Transaction,
 };
 pub use fp_rpc::TransactionStatus;
 pub use xcm_primitives::{EnsureProxy, EthereumXcmTransaction, XcmToEthereum};
 
 #[derive(PartialEq, Eq, Clone, Encode, Decode, RuntimeDebug, TypeInfo, MaxEncodedLen)]
 pub enum RawOrigin {
-	XcmEthereumTransaction(H160),
+    XcmEthereumTransaction(H160),
 }
 
 pub fn ensure_xcm_ethereum_transaction<OuterOrigin>(o: OuterOrigin) -> Result<H160, &'static str>
 where
-	OuterOrigin: Into<Result<RawOrigin, OuterOrigin>>,
+    OuterOrigin: Into<Result<RawOrigin, OuterOrigin>>,
 {
-	match o.into() {
-		Ok(RawOrigin::XcmEthereumTransaction(n)) => Ok(n),
-		_ => Err("bad origin: expected to be a xcm Ethereum transaction"),
-	}
+    match o.into() {
+        Ok(RawOrigin::XcmEthereumTransaction(n)) => Ok(n),
+        _ => Err("bad origin: expected to be a xcm Ethereum transaction"),
+    }
 }
 
 pub struct EnsureXcmEthereumTransaction;
 impl<O: Into<Result<RawOrigin, O>> + From<RawOrigin>> EnsureOrigin<O>
-	for EnsureXcmEthereumTransaction
+    for EnsureXcmEthereumTransaction
 {
-	type Success = H160;
-	fn try_origin(o: O) -> Result<Self::Success, O> {
-		o.into().map(|o| match o {
-			RawOrigin::XcmEthereumTransaction(id) => id,
-		})
-	}
+    type Success = H160;
+    fn try_origin(o: O) -> Result<Self::Success, O> {
+        o.into().map(|o| match o {
+            RawOrigin::XcmEthereumTransaction(id) => id,
+        })
+    }
 
-	#[cfg(feature = "runtime-benchmarks")]
-	fn try_successful_origin() -> Result<O, ()> {
-		Ok(O::from(RawOrigin::XcmEthereumTransaction(
-			Default::default(),
-		)))
-	}
+    #[cfg(feature = "runtime-benchmarks")]
+    fn try_successful_origin() -> Result<O, ()> {
+        Ok(O::from(RawOrigin::XcmEthereumTransaction(
+            Default::default(),
+        )))
+    }
 }
 
 pub use self::pallet::*;
 
 #[frame_support::pallet(dev_mode)]
 pub mod pallet {
-	use super::*;
-	use frame_support::pallet_prelude::*;
+    use super::*;
+    use frame_support::pallet_prelude::*;
 
-	#[pallet::config]
-	pub trait Config: frame_system::Config + pallet_timestamp::Config + pallet_evm::Config {
-		/// Invalid transaction error
-		type InvalidEvmTransactionError: From<InvalidEvmTransactionError>;
-		/// Handler for applying an already validated transaction
-		type ValidatedTransaction: ValidatedTransaction;
-		/// Origin for xcm transact
-		type XcmEthereumOrigin: EnsureOrigin<Self::RuntimeOrigin, Success = H160>;
-		/// Maximum Weight reserved for xcm in a block
-		type ReservedXcmpWeight: Get<Weight>;
-		/// Ensure proxy
-		type EnsureProxy: EnsureProxy<Self::AccountId>;
-		/// The origin that is allowed to resume or suspend the XCM to Ethereum executions.
-		type ControllerOrigin: EnsureOrigin<Self::RuntimeOrigin>;
-	}
+    #[pallet::config]
+    pub trait Config: frame_system::Config + pallet_timestamp::Config + pallet_evm::Config {
+        /// Invalid transaction error
+        type InvalidEvmTransactionError: From<InvalidEvmTransactionError>;
+        /// Handler for applying an already validated transaction
+        type ValidatedTransaction: ValidatedTransaction;
+        /// Origin for xcm transact
+        type XcmEthereumOrigin: EnsureOrigin<Self::RuntimeOrigin, Success = H160>;
+        /// Maximum Weight reserved for xcm in a block
+        type ReservedXcmpWeight: Get<Weight>;
+        /// Ensure proxy
+        type EnsureProxy: EnsureProxy<Self::AccountId>;
+        /// The origin that is allowed to resume or suspend the XCM to Ethereum executions.
+        type ControllerOrigin: EnsureOrigin<Self::RuntimeOrigin>;
+    }
 
-	#[pallet::pallet]
-	#[pallet::without_storage_info]
-	pub struct Pallet<T>(PhantomData<T>);
+    #[pallet::pallet]
+    #[pallet::without_storage_info]
+    pub struct Pallet<T>(PhantomData<T>);
 
-	/// Global nonce used for building Ethereum transaction payload.
-	#[pallet::storage]
-	#[pallet::getter(fn nonce)]
-	pub(crate) type Nonce<T: Config> = StorageValue<_, U256, ValueQuery>;
+    /// Global nonce used for building Ethereum transaction payload.
+    #[pallet::storage]
+    #[pallet::getter(fn nonce)]
+    pub(crate) type Nonce<T: Config> = StorageValue<_, U256, ValueQuery>;
 
-	/// Whether or not Ethereum-XCM is suspended from executing
-	#[pallet::storage]
-	#[pallet::getter(fn ethereum_xcm_suspended)]
-	pub(super) type EthereumXcmSuspended<T: Config> = StorageValue<_, bool, ValueQuery>;
+    /// Whether or not Ethereum-XCM is suspended from executing
+    #[pallet::storage]
+    #[pallet::getter(fn ethereum_xcm_suspended)]
+    pub(super) type EthereumXcmSuspended<T: Config> = StorageValue<_, bool, ValueQuery>;
 
-	#[pallet::origin]
-	pub type Origin = RawOrigin;
+    #[pallet::origin]
+    pub type Origin = RawOrigin;
 
-	#[pallet::error]
-	pub enum Error<T> {
-		/// Xcm to Ethereum execution is suspended
-		EthereumXcmExecutionSuspended,
-	}
+    #[pallet::error]
+    pub enum Error<T> {
+        /// Xcm to Ethereum execution is suspended
+        EthereumXcmExecutionSuspended,
+    }
 
-	#[pallet::call]
-	impl<T: Config> Pallet<T>
-	where
-		OriginFor<T>: Into<Result<RawOrigin, OriginFor<T>>>,
-	{
-		/// Xcm Transact an Ethereum transaction.
-		/// Weight: Gas limit plus the db read involving the suspension check
-		#[pallet::weight({
+    #[pallet::call]
+    impl<T: Config> Pallet<T>
+    where
+        OriginFor<T>: Into<Result<RawOrigin, OriginFor<T>>>,
+    {
+        /// Xcm Transact an Ethereum transaction.
+        /// Weight: Gas limit plus the db read involving the suspension check
+        #[pallet::weight({
 			let without_base_extrinsic_weight = false;
 			<T as pallet_evm::Config>::GasWeightMapping::gas_to_weight({
 				match xcm_transaction {
@@ -162,27 +162,27 @@ pub mod pallet {
 				}
 			}, without_base_extrinsic_weight).saturating_add(T::DbWeight::get().reads(1))
 		})]
-		pub fn transact(
-			origin: OriginFor<T>,
-			xcm_transaction: EthereumXcmTransaction,
-		) -> DispatchResultWithPostInfo {
-			let source = T::XcmEthereumOrigin::ensure_origin(origin)?;
-			ensure!(
-				!EthereumXcmSuspended::<T>::get(),
-				DispatchErrorWithPostInfo {
-					error: Error::<T>::EthereumXcmExecutionSuspended.into(),
-					post_info: PostDispatchInfo {
-						actual_weight: Some(T::DbWeight::get().reads(1)),
-						pays_fee: Pays::Yes
-					}
-				}
-			);
-			Self::validate_and_apply(source, xcm_transaction)
-		}
+        pub fn transact(
+            origin: OriginFor<T>,
+            xcm_transaction: EthereumXcmTransaction,
+        ) -> DispatchResultWithPostInfo {
+            let source = T::XcmEthereumOrigin::ensure_origin(origin)?;
+            ensure!(
+                !EthereumXcmSuspended::<T>::get(),
+                DispatchErrorWithPostInfo {
+                    error: Error::<T>::EthereumXcmExecutionSuspended.into(),
+                    post_info: PostDispatchInfo {
+                        actual_weight: Some(T::DbWeight::get().reads(1)),
+                        pays_fee: Pays::Yes
+                    }
+                }
+            );
+            Self::validate_and_apply(source, xcm_transaction)
+        }
 
-		/// Xcm Transact an Ethereum transaction through proxy.
-		/// Weight: Gas limit plus the db reads involving the suspension and proxy checks
-		#[pallet::weight({
+        /// Xcm Transact an Ethereum transaction through proxy.
+        /// Weight: Gas limit plus the db reads involving the suspension and proxy checks
+        #[pallet::weight({
 			let without_base_extrinsic_weight = false;
 			<T as pallet_evm::Config>::GasWeightMapping::gas_to_weight({
 				match xcm_transaction {
@@ -191,119 +191,119 @@ pub mod pallet {
 				}
 			}, without_base_extrinsic_weight).saturating_add(T::DbWeight::get().reads(2))
 		})]
-		pub fn transact_through_proxy(
-			origin: OriginFor<T>,
-			transact_as: H160,
-			xcm_transaction: EthereumXcmTransaction,
-		) -> DispatchResultWithPostInfo {
-			let source = T::XcmEthereumOrigin::ensure_origin(origin)?;
-			ensure!(
-				!EthereumXcmSuspended::<T>::get(),
-				DispatchErrorWithPostInfo {
-					error: Error::<T>::EthereumXcmExecutionSuspended.into(),
-					post_info: PostDispatchInfo {
-						actual_weight: Some(T::DbWeight::get().reads(1)),
-						pays_fee: Pays::Yes
-					}
-				}
-			);
-			let _ = T::EnsureProxy::ensure_ok(
-				T::AddressMapping::into_account_id(transact_as),
-				T::AddressMapping::into_account_id(source),
-			)
-			.map_err(|e| sp_runtime::DispatchErrorWithPostInfo {
-				post_info: PostDispatchInfo {
-					actual_weight: Some(T::DbWeight::get().reads(2)),
-					pays_fee: Pays::Yes,
-				},
-				error: sp_runtime::DispatchError::Other(e),
-			})?;
+        pub fn transact_through_proxy(
+            origin: OriginFor<T>,
+            transact_as: H160,
+            xcm_transaction: EthereumXcmTransaction,
+        ) -> DispatchResultWithPostInfo {
+            let source = T::XcmEthereumOrigin::ensure_origin(origin)?;
+            ensure!(
+                !EthereumXcmSuspended::<T>::get(),
+                DispatchErrorWithPostInfo {
+                    error: Error::<T>::EthereumXcmExecutionSuspended.into(),
+                    post_info: PostDispatchInfo {
+                        actual_weight: Some(T::DbWeight::get().reads(1)),
+                        pays_fee: Pays::Yes
+                    }
+                }
+            );
+            let _ = T::EnsureProxy::ensure_ok(
+                T::AddressMapping::into_account_id(transact_as),
+                T::AddressMapping::into_account_id(source),
+            )
+            .map_err(|e| sp_runtime::DispatchErrorWithPostInfo {
+                post_info: PostDispatchInfo {
+                    actual_weight: Some(T::DbWeight::get().reads(2)),
+                    pays_fee: Pays::Yes,
+                },
+                error: sp_runtime::DispatchError::Other(e),
+            })?;
 
-			Self::validate_and_apply(transact_as, xcm_transaction)
-		}
+            Self::validate_and_apply(transact_as, xcm_transaction)
+        }
 
-		/// Suspends all Ethereum executions from XCM.
-		///
-		/// - `origin`: Must pass `ControllerOrigin`.
-		#[pallet::weight((T::DbWeight::get().writes(1), DispatchClass::Operational,))]
-		pub fn suspend_ethereum_xcm_execution(origin: OriginFor<T>) -> DispatchResult {
-			T::ControllerOrigin::ensure_origin(origin)?;
+        /// Suspends all Ethereum executions from XCM.
+        ///
+        /// - `origin`: Must pass `ControllerOrigin`.
+        #[pallet::weight((T::DbWeight::get().writes(1), DispatchClass::Operational,))]
+        pub fn suspend_ethereum_xcm_execution(origin: OriginFor<T>) -> DispatchResult {
+            T::ControllerOrigin::ensure_origin(origin)?;
 
-			EthereumXcmSuspended::<T>::put(true);
+            EthereumXcmSuspended::<T>::put(true);
 
-			Ok(())
-		}
+            Ok(())
+        }
 
-		/// Resumes all Ethereum executions from XCM.
-		///
-		/// - `origin`: Must pass `ControllerOrigin`.
-		#[pallet::weight((T::DbWeight::get().writes(1), DispatchClass::Operational,))]
-		pub fn resume_ethereum_xcm_execution(origin: OriginFor<T>) -> DispatchResult {
-			T::ControllerOrigin::ensure_origin(origin)?;
+        /// Resumes all Ethereum executions from XCM.
+        ///
+        /// - `origin`: Must pass `ControllerOrigin`.
+        #[pallet::weight((T::DbWeight::get().writes(1), DispatchClass::Operational,))]
+        pub fn resume_ethereum_xcm_execution(origin: OriginFor<T>) -> DispatchResult {
+            T::ControllerOrigin::ensure_origin(origin)?;
 
-			EthereumXcmSuspended::<T>::put(false);
+            EthereumXcmSuspended::<T>::put(false);
 
-			Ok(())
-		}
-	}
+            Ok(())
+        }
+    }
 }
 
 impl<T: Config> Pallet<T> {
-	fn validate_and_apply(
-		source: H160,
-		xcm_transaction: EthereumXcmTransaction,
-	) -> DispatchResultWithPostInfo {
-		// The lack of a real signature where different callers with the
-		// same nonce are providing identical transaction payloads results in a collision and
-		// the same ethereum tx hash.
-		// We use a global nonce instead the user nonce for all Xcm->Ethereum transactions to avoid
-		// this.
-		let current_nonce = Self::nonce();
-		let error_weight = T::DbWeight::get().reads(1);
+    fn validate_and_apply(
+        source: H160,
+        xcm_transaction: EthereumXcmTransaction,
+    ) -> DispatchResultWithPostInfo {
+        // The lack of a real signature where different callers with the
+        // same nonce are providing identical transaction payloads results in a collision and
+        // the same ethereum tx hash.
+        // We use a global nonce instead the user nonce for all Xcm->Ethereum transactions to avoid
+        // this.
+        let current_nonce = Self::nonce();
+        let error_weight = T::DbWeight::get().reads(1);
 
-		let transaction: Option<Transaction> =
-			xcm_transaction.into_transaction_v2(current_nonce, T::ChainId::get());
-		if let Some(transaction) = transaction {
-			let transaction_data: TransactionData = (&transaction).into();
+        let transaction: Option<Transaction> =
+            xcm_transaction.into_transaction_v2(current_nonce, T::ChainId::get());
+        if let Some(transaction) = transaction {
+            let transaction_data: TransactionData = (&transaction).into();
 
-			let _ = CheckEvmTransaction::<T::InvalidEvmTransactionError>::new(
-				CheckEvmTransactionConfig {
-					evm_config: T::config(),
-					block_gas_limit: U256::from(
-						<T as pallet_evm::Config>::GasWeightMapping::weight_to_gas(
-							T::ReservedXcmpWeight::get(),
-						),
-					),
-					base_fee: U256::zero(),
-					chain_id: 0u64,
-					is_transactional: true,
-				},
-				transaction_data.into(),
-			)
-			// We only validate the gas limit against the evm transaction cost.
-			// No need to validate fee payment, as it is handled by the xcm executor.
-			.validate_common()
-			.map_err(|_| sp_runtime::DispatchErrorWithPostInfo {
-				post_info: PostDispatchInfo {
-					actual_weight: Some(error_weight),
-					pays_fee: Pays::Yes,
-				},
-				error: sp_runtime::DispatchError::Other("Failed to validate ethereum transaction"),
-			})?;
+            let _ = CheckEvmTransaction::<T::InvalidEvmTransactionError>::new(
+                CheckEvmTransactionConfig {
+                    evm_config: T::config(),
+                    block_gas_limit: U256::from(
+                        <T as pallet_evm::Config>::GasWeightMapping::weight_to_gas(
+                            T::ReservedXcmpWeight::get(),
+                        ),
+                    ),
+                    base_fee: U256::zero(),
+                    chain_id: 0u64,
+                    is_transactional: true,
+                },
+                transaction_data.into(),
+            )
+            // We only validate the gas limit against the evm transaction cost.
+            // No need to validate fee payment, as it is handled by the xcm executor.
+            .validate_common()
+            .map_err(|_| sp_runtime::DispatchErrorWithPostInfo {
+                post_info: PostDispatchInfo {
+                    actual_weight: Some(error_weight),
+                    pays_fee: Pays::Yes,
+                },
+                error: sp_runtime::DispatchError::Other("Failed to validate ethereum transaction"),
+            })?;
 
-			// Once we know a new transaction hash exists - the user can afford storing the
-			// transaction on chain - we increase the global nonce.
-			<Nonce<T>>::put(current_nonce.saturating_add(U256::one()));
+            // Once we know a new transaction hash exists - the user can afford storing the
+            // transaction on chain - we increase the global nonce.
+            <Nonce<T>>::put(current_nonce.saturating_add(U256::one()));
 
-			T::ValidatedTransaction::apply(source, transaction)
-		} else {
-			Err(sp_runtime::DispatchErrorWithPostInfo {
-				post_info: PostDispatchInfo {
-					actual_weight: Some(error_weight),
-					pays_fee: Pays::Yes,
-				},
-				error: sp_runtime::DispatchError::Other("Cannot convert xcm payload to known type"),
-			})
-		}
-	}
+            T::ValidatedTransaction::apply(source, transaction)
+        } else {
+            Err(sp_runtime::DispatchErrorWithPostInfo {
+                post_info: PostDispatchInfo {
+                    actual_weight: Some(error_weight),
+                    pays_fee: Pays::Yes,
+                },
+                error: sp_runtime::DispatchError::Other("Cannot convert xcm payload to known type"),
+            })
+        }
+    }
 }
